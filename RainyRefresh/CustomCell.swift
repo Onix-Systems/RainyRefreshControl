82dcafbc9e2f91725d9769a3a4f14a4f9263d4e9
Alexei <am.nezhyborets@gmail.com>
2016-11-24 21:22:31 +0200

    Update CustomCell.swift

--- RainyRefresh/CustomCell.swift
+++ RainyRefresh/CustomCell.swift
@@ -14,19 +14,6 @@ class CustomCell: UITableViewCell {
     @IBOutlet weak var firstLinePlaceholder: UIView!
     @IBOutlet weak var secondLinePlaceholder: UIView!
     @IBOutlet weak var thirdLinePlaceholder: UIView!
-    
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
 
 extension CustomCell {
